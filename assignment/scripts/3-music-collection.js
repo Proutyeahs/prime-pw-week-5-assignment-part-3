92e6514a69f6b9a765614138d1ca1a3350d5c316
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-07-05 18:43:17 -0500

    started assignment

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,11 @@
 console.log('***** Music Collection *****')
+
+let collection = []
+
+function addToCollection(title, artist, yearPublished) {
+    this.title = title
+    this.artist = artist
+    this.yearPublished = yearPublished
+}
+
+console.log(album)
