5f8cdc08dd0d3842e8a896ea6f1ae4acfd6b7d71
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-07-06 13:00:18 -0500

    Testing alt formats

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,19 +3,19 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-    let album = {
-        title: title,
-        artist: artist,
-        yearPublished: yearPublished
-    }
+    let album = {};
+    album.title = title;
+    album.artist = artist;
+    album.yearPublished = yearPublished;
     collection.push(album);
     return console.log(album);
 }
 
-addToCollection('Samurai Champloo', 'tsutchie', 2004)
-addToCollection('THE GOAT', 'Polo G', 2020)
-addToCollection('By Your Side', 'Breakbot', 2012)
-addToCollection('American IV: The Man Comes Around', 'Johnny Cash', 2002)
-addToCollection('In Return', 'ODESZA', 2014)
-addToCollection('The Eminem Show', 'Eminem', 2002)
-console.log(collection)
+addToCollection('Samurai Champloo', 'tsutchie', 2004);
+addToCollection('THE GOAT', 'Polo G', 2020);
+addToCollection('By Your Side', 'Breakbot', 2012);
+addToCollection('American IV: The Man Comes Around', 'Johnny Cash', 2002);
+addToCollection('In Return', 'ODESZA', 2014);
+addToCollection('The Eminem Show', 'Eminem', 2002);
+
+console.log(collection);
