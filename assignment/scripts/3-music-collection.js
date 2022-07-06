e88bde74bc523a3b6e8d30ea2f7340959229bc36
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-07-06 12:54:44 -0500

    addToCollection function complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,21 @@
 console.log('***** Music Collection *****')
 
-let collection = []
+let collection = [];
 
 function addToCollection(title, artist, yearPublished) {
-    this.title = title
-    this.artist = artist
-    this.yearPublished = yearPublished
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return console.log(album);
 }
 
-console.log(album)
+addToCollection('Samurai Champloo', 'tsutchie', 2004)
+addToCollection('THE GOAT', 'Polo G', 2020)
+addToCollection('By Your Side', 'Breakbot', 2012)
+addToCollection('American IV: The Man Comes Around', 'Johnny Cash', 2002)
+addToCollection('In Return', 'ODESZA', 2014)
+addToCollection('The Eminem Show', 'Eminem', 2002)
+console.log(collection)
