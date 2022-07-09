e457dd5326f141115ee1938103a0619f0de706b7
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-07-08 22:59:17 -0500

    This is not working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,21 +2,22 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, ...array) {
     let album = {};
     album.title = title;
     album.artist = artist;
     album.yearPublished = yearPublished;
+    album.tracks = array;
     collection.push(album);
     return console.log(album);
 }
 
-addToCollection('Self Inflicted', 'Chelsea Grin', 2016);
-addToCollection('THE GOAT', 'Polo G', 2020);
-addToCollection('By Your Side', 'Breakbot', 2012);
-addToCollection('American IV: The Man Comes Around', 'Johnny Cash', 2002);
-addToCollection('In Return', 'ODESZA', 2014);
-addToCollection('The Eminem Show', 'Eminem', 2002);
+addToCollection('Self Inflicted', 'Chelsea Grin', 2016, ["Scratching and Screaming", 3.23], ["Never, Forever", 4.00]);
+addToCollection('THE GOAT', 'Polo G', 2020, ["Beautiful Pain", 2.51], ["21", 2.43], ["Martin & Gina", 2.12]);
+addToCollection('By Your Side', 'Breakbot', 2012, ["Baby Im Yours", 3.36]);
+addToCollection('American IV: The Man Comes Around', 'Johnny Cash', 2002, ["Hurt", 3.36], ["I Hung My Head", 3.53]);
+addToCollection('In Return', 'ODESZA', 2014, ["Always This Late", 2.40], ["Say My Name", 4.22], ["Sundara", 2.16], ["Sun Models", 2.40]);
+addToCollection('The Eminem Show', 'Eminem', 2002, ["Without Me", 4.50], ["Superman", 5.50], ["Till I Collapse", 4.57]);
 
 console.log(collection);
 
@@ -44,9 +45,12 @@ findByArtist('Eminem');
 findByArtist('Polo G');
 findByArtist("Eric Prouty");
 
-function search(artist, yearPublished) {
+function search(artist, yearPublished, trackName) {
     let collectionItems = [];
     for (let i = 0; i < collection.length; i++) {
+        if (trackName === collection[i].tracks) {
+            return console.log(trackName)
+        }
         if (artist === collection[i].artist && collection[i].yearPublished === yearPublished) {
             collectionItems.push(artist, yearPublished);
             return console.log(collectionItems);
@@ -58,6 +62,7 @@ function search(artist, yearPublished) {
     }
 }
 
+search("Breakbot", 2012, "Baby Im Yours")
 search("Eminem", 2002);
 search("Johnny Cash", 2002);
 search("ODESZA");
